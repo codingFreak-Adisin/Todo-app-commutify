7302d917637b760b669a5a400021a2f766a8083b
Aditya Sinha <70958307+codingFreak-Adisin@users.noreply.github.com>
2020-11-08 14:52:31 +0530

    Add files via upload

---
+++ Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA2w00u5mtP_zScXYzf-GU6oNUoVrjeaLs",
+    authDomain: "todo-app-commutify-page.firebaseapp.com",
+    databaseURL: "https://todo-app-commutify-page.firebaseio.com",
+    projectId: "todo-app-commutify-page",
+    storageBucket: "todo-app-commutify-page.appspot.com",
+    messagingSenderId: "1085365824511",
+    appId: "1:1085365824511:web:3614d3ae8499fd8b4755ca",
+    measurementId: "G-E70V43ED38"
+};
